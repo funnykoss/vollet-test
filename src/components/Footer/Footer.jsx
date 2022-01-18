bbf5c1c4088a78eacdefb3edb3d6d2e9ab758d9a
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-18 22:06:42 +0200

    footer footerModules

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FooterNavigation from '../FooterNavigation'
+import SubscriptionForm from "../SubscriptionForm";
+
+
+const Footer = () => {
+    return (
+        <>
+            <FooterNavigation />
+            <SubscriptionForm/>
+        </>
+    )
+}
+
+export default Footer
