5c0bc77b965af8c38504dde97a5a28908da901b6
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-18 22:06:08 +0200

    app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
